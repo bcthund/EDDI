bcthund/EDDI

commit ccc7e1bf75515b580faa11fee407fcaed13a71fe

Use event batches / groups to extract additional context from events
Revise journal log monitoring so that we can handle a series of events written in the same event batch and can learn information from the patterns of those events.

JournalMonitor/LogMonitor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -12,10 +13,10 @@ namespace EddiJournalMonitor
     public class LogMonitor
     {
         // What we are monitoring and what to do with it
-        public string Directory;
-        public Regex Filter;
-        public Action<string, bool> Callback;
-        public static string journalFileName;
+        private string Directory;
+        private Regex Filter;
+        private Action<IEnumerable<string>, bool> Callback;
+        protected static string journalFileName;
 
         private const int pollingIntervalActiveMs = 100;
         private const int pollingIntervalRelaxedMs = 5000;
@@ -25,14 +26,14 @@ namespace EddiJournalMonitor
 
         public LogMonitor(string filter) { Filter = new Regex(filter); }
 
-        public LogMonitor(string directory, string filter, Action<string, bool> callback)
+        protected LogMonitor(string directory, string filter, Action<IEnumerable<string>, bool> callback)
         {
             Directory = directory;
             Filter = new Regex(filter);
             Callback = callback;
         }
 
-        public void start(bool readAllOnLoad = false)
+        protected void start(bool readAllOnLoad = false)
         {
             if (Directory == null || Directory.Trim() == "")
             {
@@ -41,7 +42,7 @@ namespace EddiJournalMonitor
 
             running = true;
             long lastSize = 0;
-            bool activePolling = false;
+            var activePolling = false;
 
             // Main loop
             while (running)
@@ -78,15 +79,15 @@ namespace EddiJournalMonitor
                 else if (fileInfo.Name != journalFileName)
                 {
                     // We have found a player journal file that is fresher than the one we are using
-                    bool isFirstLoad = journalFileName == null;
+                    var isFirstLoad = journalFileName == null;
                     journalFileName = fileInfo.Name;
                     lastSize = fileInfo.Length;
 
                     if (readAllOnLoad)
                     {
                         // Read everything in the file into the journal monitor
-                        long seekPos = 0;
-                        int readLen = (int)fileInfo.Length;
+                        const long seekPos = 0;
+                        var readLen = (int)fileInfo.Length;
                         Read(seekPos, readLen, fileInfo, isFirstLoad);
                     }
                     else
@@ -100,8 +101,8 @@ namespace EddiJournalMonitor
                     // The player journal file in memory is the correct file. Look for new journal events
                     journalFileName = fileInfo.Name;
 
-                    long thisSize = fileInfo.Length;
-                    int readLen = 0;
+                    var thisSize = fileInfo.Length;
+                    var readLen = 0;
                     long seekPos = 0;
 
                     if (lastSize != thisSize)
@@ -128,26 +129,20 @@ namespace EddiJournalMonitor
 
         private void Read(long seekPos, int readLen, FileInfo fileInfo, bool isLoadEvent)
         {
-            using (FileStream fs = fileInfo.Open(FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var fs = fileInfo.Open(FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             {
                 fs.Seek(seekPos, SeekOrigin.Begin);
-                byte[] bytes = new byte[readLen];
-                int haveRead = 0;
+                var bytes = new byte[readLen];
+                var haveRead = 0;
                 while (haveRead < readLen)
                 {
                     haveRead += fs.Read(bytes, haveRead, readLen - haveRead);
                     fs.Seek(seekPos + haveRead, SeekOrigin.Begin);
                 }
                 // Convert bytes to string
-                string s = Encoding.UTF8.GetString(bytes);
-                string[] lines = Regex.Split(s, "\r?\n");
-                foreach (string line in lines)
-                {
-                    if (line != "")
-                    {
-                        Callback(line, isLoadEvent);
-                    }
-                }
+                var s = Encoding.UTF8.GetString(bytes);
+                var lines = Regex.Split(s, "\r?\n");
+                Callback( lines.Where( l => l != "" ), isLoadEvent );
             }
         }
 
@@ -155,51 +150,48 @@ namespace EddiJournalMonitor
         {
             long seekPos = 0;
 
-            using (FileStream fs = fileInfo.Open(FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var fs = fileInfo.Open(FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             {
                 fs.Seek(seekPos, SeekOrigin.Begin);
-                byte[] bytes = new byte[fileInfo.Length];
-                int haveRead = 0;
+                var bytes = new byte[fileInfo.Length];
+                var haveRead = 0;
                 while (haveRead < fileInfo.Length)
                 {
                     haveRead += fs.Read(bytes, haveRead, (int)fileInfo.Length - haveRead);
                     fs.Seek(seekPos + haveRead, SeekOrigin.Begin);
                 }
                 // Convert bytes to strings
-                string s = Encoding.UTF8.GetString(bytes);
-                string[] lines = Regex.Split(s, "\r?\n");
+                var s = Encoding.UTF8.GetString(bytes);
+                var lines = Regex.Split(s, "\r?\n")
+                    .Select( (v, i) => new { Key = i, Value = v })
+                    .ToDictionary(kv => kv.Key, kv => kv.Value );
 
                 // First line should be a file header
-                string firstLine = lines.FirstOrDefault();
+                var firstLine = lines.Any() ? lines.FirstOrDefault().Value : null;
                 if (!string.IsNullOrEmpty(firstLine) && firstLine.Contains("Fileheader"))
                 {
                     // Pass this along as an event
-                    Callback(firstLine, isLoadEvent);
+                    Callback( new[] { firstLine }, isLoadEvent);
                 }
 
                 // Find the latest "Commander" event, written at the start of the Load Game process
                 // (whenever loading from the main menu) 
-                var commanderLoadLines = lines
-                    .Select((text, index) => new { line = text, lineNumber = index })
-                    .Where(x => x.line.Contains(@"""event"":""Commander"""));
-                var lastLoadLine = commanderLoadLines.LastOrDefault();
+                var lastLoadLine = lines
+                    .LastOrDefault( x => x.Value.Contains(@"""event"":""Commander""") );
 
-                if (lastLoadLine != null)
+                if (lastLoadLine.Value is null)
                 {
-                    Task.Run(() => {
-                        for (int i = lastLoadLine.lineNumber; i < lines.Count(); i++)
-                        {
-                            if (lines[i] != "")
-                            {
-                                Callback(lines[i], isLoadEvent);
-                            }
-                        }
+                    Task.Run(() =>
+                    {
+                        Callback( lines
+                            .Where(l => l.Key >= lastLoadLine.Key )
+                            .Select(l => l.Value), isLoadEvent );
                     });
                 }
             }
         }
 
-        public void stop()
+        protected void stop()
         {
             running = false;
         }

JournalMonitor/JournalMonitor.cs
@@ -3,6 +3,7 @@ using EddiCore;
 using EddiDataDefinitions;
 using EddiDataProviderService;
 using EddiEvents;
+using JetBrains.Annotations;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
@@ -23,48 +24,45 @@ namespace EddiJournalMonitor
 
         public JournalMonitor () : base( GetSavedGamesDir(), @"^Journal.*\.[0-9\.]+\.log$",
             ( result, isLogLoadEvent ) =>
-                ForwardJournalEntry( result, EDDI.Instance.enqueueEvent, isLogLoadEvent ) )
+                ForwardJournalEntries( result.ToList(), EDDI.Instance.enqueueEvent, isLogLoadEvent ) )
         { }
 
-        private enum ShipyardType { ShipsHere, ShipsRemote }
+        private enum ShipyardType { [UsedImplicitly] ShipsHere, [UsedImplicitly] ShipsRemote }
 
         private static readonly Dictionary<long, CancellationTokenSource> carrierJumpCancellationTokenSources =
             new Dictionary<long, CancellationTokenSource>();
 
         private static Task ShipShutdownTask = null;
 
-        public static void ForwardJournalEntry(string line, Action<Event> callback, bool isLogLoadEvent)
+        private static void ForwardJournalEntries ( List<string> lines, Action<Event> callback, bool isLogLoadEventBatch )
         {
-            if (line == null)
-            {
-                return;
+            if ( !lines.Any() ) { return; }
+
+            var events = ParseJournalEntries(lines, isLogLoadEventBatch);
+
+            // Enqueue events for processing
+            events.ForEach(callback);
             }
 
-            List<Event> events = ParseJournalEntry(line, isLogLoadEvent);
-            foreach (Event @event in events)
+        public static List<Event> ParseJournalEntries(IEnumerable<string> lines, bool fromLogLoad = false)
             {
-                // The DiscoveryScanEvent and SystemScanComplete events may be written before all applicable scans have been queued.
-                // We will wait a short period of time after these events take place so that all scans generated in tandem 
-                // with these events are enqueued before these events are enqueued.
-                if ((@event is DiscoveryScanEvent || @event is SystemScanComplete) && !@event.fromLoad)
-                {
-                    Task.Run(async () =>
-                    {
-                        int timeout = 0;
-                        do
-                        {
-                            await Task.Delay(1500);
-                            timeout++;
-                        }
-                        while (EDDI.Instance.CurrentStarSystem?.bodies.Count == 0 && timeout < 3);
-                        callback(@event);
-                    });
-                    continue;
-                }
+            var events = lines
+                .Where( line => !string.IsNullOrEmpty( line ) )
+                .SelectMany( line => ParseJournalEntry( line, fromLogLoad ) )
+                .ToList();
 
-                callback(@event);
+            if ( fromLogLoad ) { return events; }
+
+            // Reorder DiscoveryScanEvent to occur after other events in the batch including body and star scans
+            var discoveryScanEvents = events.OfType<DiscoveryScanEvent>().ToList();
+            events = events.Except( discoveryScanEvents ).Concat( discoveryScanEvents ).ToList();
+
+            // Reorder SystemScanComplete to occur after other events in the batch including body and star scans
+            var SystemScanCompleteEvents = events.OfType<SystemScanComplete>().ToList();
+            events = events.Except( SystemScanCompleteEvents ).Concat( SystemScanCompleteEvents ).ToList();
+
+            return events;
             }
-        }
 
         public static List<Event> ParseJournalEntry(string line, bool fromLogLoad = false)
         {

commit 6d597f289bf5b1fff5ff4ca018db4ed1b1d4065f

Fix a bug introduced in ccc7e1bf75515b580faa11fee407fcaed13a71fe which was preventing log loading.

JournalMonitor/LogMonitor.cs
@@ -179,7 +179,7 @@ namespace EddiJournalMonitor
                 var lastLoadLine = lines
                     .LastOrDefault( x => x.Value.Contains(@"""event"":""Commander""") );
 
-                if (lastLoadLine.Value is null)
+                if (lastLoadLine.Value != null)
                 {
                     Task.Run(() =>
                     {