ef0e0e3abcd467b9b0bf90d5be6bec21da0c82d1
T'kael <DragonCharmer84@hotmail.com>
2024-06-23 19:42:21 -0700

    Make log redaction slightly more efficient.

--- Utilities/Redaction.cs
+++ Utilities/Redaction.cs
@@ -60,14 +60,12 @@ namespace Utilities
         {
             if ( data is JObject objectData )
             {
-                foreach ( string property in PersonalProperties )
-                {
-                    objectData.Descendants()
-                        .OfType<JProperty>()
-                        .Where( attr => attr.Name.StartsWith( property, StringComparison.OrdinalIgnoreCase ) )
-                        .ToList()
-                        .ForEach( attr => attr.Remove() );
-                }
+                objectData.Descendants()
+                    .OfType<JProperty>()
+                    .Where( attr => PersonalProperties.Any( property =>
+                        attr.Name.StartsWith( property, StringComparison.OrdinalIgnoreCase ) ) )
+                    .ToList()
+                    .ForEach( attr => attr.Remove() );
                 return objectData;
             }
             if ( data is JArray arrayData )
